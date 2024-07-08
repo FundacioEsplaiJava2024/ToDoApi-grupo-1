cfd678bebeeb94167d4316b4bbaa58d60683c851
AngelCorral0 <angel.corral.olivares@gmail.com>
2024-07-08 13:08:21 +0200

    constructor creado

--- todo-api/src/main/java/com/example/todo_api/entity/toDo.java
+++ todo-api/src/main/java/com/example/todo_api/entity/toDo.java
@@ -1,37 +1,83 @@
+package com.example.todo_api.entity;
+
+import java.util.Date;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name ="todos")
-
+@Table(name = "todos")
 
 public class toDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
-    private String name;
+    private String title;
+    private String description;
+    private Date dataCreated;
+    private Date deadLine;
+    private String priority;
+    private Boolean isDoing;
 
     public toDo() {
 
     }
 
-    public toDo(String name) {
-        this.name = name;
+    public toDo(String title, String description, Date dataCreated, Date deadLine, String priority, Boolean isDoing) {
+        this.title = title;
+        this.description = description;
+        this.dataCreated = dataCreated;
+        this.deadLine = deadLine;
+        this.priority = priority;
+        this.isDoing = isDoing;
     }
 
     public Long getId() {
         return id;
-
     }
+    public String gettitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public Date getDataCreated() {
+        return dataCreated;
+    }
+    public Date getDeadLine() {
+        return deadLine;
+    }
+    public String getPriority() {
+        return priority;
+    }
+    public Boolean getIsDoing() {
+        return isDoing;
+    }
+
 
     public void setId(long id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
+    public void settitle(String title) {
+        this.title = title;
     }
-
-    public void setNAme( String name){
-        
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setDataCreated(Date dataCreated){
+        this.dataCreated = dataCreated;
+    }
+    public void setDeadLine(Date deadLine){
+        this.deadLine = deadLine;
+    }
+    public void setPriority(String priority){
+        this.priority = priority;
+    }
+    public void setIsDoing(Boolean isDoing){
+        this.isDoing = isDoing;
     }
 
 }
